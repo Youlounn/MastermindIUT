046cc5763d881178f1c6793bb0b982219a30bbce
KONFeature <quentin@nivelais.com>
2016-11-18 10:02:33 +0100

    Update controleurConnection.php

--- controleur/controleurConnection.php
+++ controleur/controleurConnection.php
@@ -5,20 +5,24 @@ class ControleurConnection{
   private $bd;
 
   function __construct(){
-    $this->bd = new Bd();
+    try {
+      $this->bd = new Bd();
+    } catch (MonException $e){
+      return $e->afficher();
+    }
   }
 
   function connection($nom,$mdp){
     $ret = false;
     try {
       $passBDD = $this->bd->getMdp($nom);
-    } catch (UserException $e) {
-      $ret = $e;
-    }
-    if (crypt($mdp,$passBDD)==$passBDD) {
-      $ret = "ok";
-    }else {
-      $ret = "Erreur dans pseudo ou mot de passe";
+      if (crypt($mdp,$passBDD)==$passBDD) {
+        $ret = "ok";
+      }else {
+        $ret = "Erreur dans pseudo ou mot de passe";
+      }
+    } catch (MonException $e) {
+      $ret = $e->afficher();
     }
     return $ret;
   }
